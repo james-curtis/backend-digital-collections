d6aff7b487f3bc7ba9d5760aabc95712c38ff180
JamesCurtis <jamescurtis@foxmail.com>
2022-08-29 16:30:17 +0800

    chore: init

---
+++ public/assets/less/kod/config/version.php
@@ -0,0 +1,3 @@
+<?php
+define('KOD_VERSION','4.46');
+define('KOD_VERSION_BUILD','0714');//time(),0409
---
+++ public/assets/less/kod/data/system/system_member.php
@@ -0,0 +1,53 @@
+<?php exit;?>{
+    "1": {
+        "name": "admin",
+        "path": "admin",
+        "password": "922a94abad4e12efa60bf6379b6800f2",
+        "userID": "1",
+        "role": "1",
+        "config": {
+            "sizeMax": "0",
+            "sizeUse": 1024
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "createTime": 1660275359,
+        "status": 1,
+        "lastLogin": 1660735240
+    },
+    "100": {
+        "userID": "100",
+        "name": "demo",
+        "password": "fe01ce2a7fbac8fafaed7c982a04e229",
+        "role": "2",
+        "config": {
+            "sizeMax": 5,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "demo",
+        "status": 1,
+        "lastLogin": "",
+        "createTime": 1660275359
+    },
+    "101": {
+        "userID": "101",
+        "name": "guest",
+        "password": "084e0343a0486ff05530df6c705c8bb4",
+        "role": "100",
+        "config": {
+            "sizeMax": 0.1,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "read"
+        },
+        "path": "guest",
+        "status": 1,
+        "lastLogin": "",
+        "createTime": 1660275359
+    }
+}
