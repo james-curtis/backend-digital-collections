166b011d8319188cd7648db9f9d059d6811c2375
JamesCurtis <jamescurtis@foxmail.com>
2022-09-07 14:03:58 +0800

    fix: 海报二维码错位&保存

--- public/index.php
+++ public/index.php
@@ -12,22 +12,22 @@
 // [ 应用入口文件 ]
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
-$origin = isset($_SERVER['HTTP_ORIGIN'])? $_SERVER['HTTP_ORIGIN'] : '';
-$allow_origin = array(
-    'http://metaq.pro',
-    'http://www.metaq.pro',
-    'https://metaq.pro',
-    'https://www.metaq.pro',
-);
-header('Access-Control-Allow-Origin: *');
+$origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
+//$allow_origin = array(
+//    'http://metaq.pro',
+//    'http://www.metaq.pro',
+//    'https://metaq.pro',
+//    'https://www.metaq.pro',
+//);
+//header('Access-Control-Allow-Origin: *');
 //if(in_array($origin, $allow_origin)){
 //    header('Access-Control-Allow-Origin:'.$origin);
 //}
-header("Access-Control-Allow-Methods:GET,POST,OPTIONS,DELETE");
-header("Access-Control-Allow-Credentials:true");
-header("Access-Control-Allow-Headers:DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type, Accept-Language, Origin, Accept-Encoding,token,language_id,Cookies,Cookie");
+//header("Access-Control-Allow-Methods:GET,POST,OPTIONS,DELETE");
+//header("Access-Control-Allow-Credentials:true");
+//header("Access-Control-Allow-Headers:DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type, Accept-Language, Origin, Accept-Encoding,token,language_id,Cookies,Cookie");
 
-if($_SERVER['REQUEST_METHOD'] == 'OPTIONS'){
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     exit;
 }
 // 判断是否安装
