ed438160c4520467d783f1f20c2aab63696d4d30
JamesCurtis <jamescurtis@foxmail.com>
2022-09-21 18:56:24 +0800

    feat: 提现记录导出excel
    link redrose-office/digital-collections#74
    close redrose-office/digital-collections#76

--- application/admin/controller/DrawRecord.php
+++ application/admin/controller/DrawRecord.php
@@ -5,6 +5,12 @@ namespace app\admin\controller;
 use app\common\controller\Backend;
 use logicmodel\AccountLogic;
 use logicmodel\MemberLogic;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 use think\Db;
 use think\Session;
 
@@ -136,5 +142,137 @@ class DrawRecord extends Backend
         }
         $this->assign('ids',$ids);
         return $this->fetch();
+    }
+
+
+
+    public function export()
+    {
+        if ($this->request->isPost()) {
+            set_time_limit(0);
+            $search = $this->request->post('search');
+            $ids = $this->request->post('ids');
+            $filter = $this->request->post('filter');
+            $op = $this->request->post('op');
+            $columns = $this->request->post('columns');
+
+
+            $excel = new Spreadsheet();
+            $excel->getProperties()
+                ->setCreator("FastAdmin")
+                ->setLastModifiedBy("FastAdmin")
+                ->setTitle("标题")
+                ->setSubject("Subject");
+            $excel->getDefaultStyle()->getFont()->setName('Microsoft Yahei');
+            $excel->getDefaultStyle()->getFont()->setSize(12);
+            $excel->getDefaultStyle()->applyFromArray(
+                array(
+                    'fill' => array(
+                        'type' => Fill::FILL_SOLID,
+                        'color' => array('rgb' => '000000')
+                    ),
+                    'font' => array(
+                        'color' => array('rgb' => "000000"),
+                    ),
+                    'alignment' => array(
+                        'vertical' => Alignment::VERTICAL_CENTER,
+                        'horizontal' => Alignment::HORIZONTAL_CENTER,
+                        'indent' => 1
+                    ),
+                    'borders' => array(
+                        'allborders' => array('style' => Border::BORDER_THIN),
+                    )
+                ));
+
+
+            $worksheet = $excel->setActiveSheetIndex(0);
+            $worksheet->setTitle('标题');
+            $whereIds = $ids == 'all' ? '1=1' : ['draw_record.id' => ['in', explode(',', $ids)]];
+            $this->request->get(['search' => $search, 'ids' => $ids, 'filter' => $filter, 'op' => $op]);
+            list($where, $sort, $order, $offset, $limit) = $this->buildparams();
+            $line = 1;
+            $list = [];
+            $columns_arr = explode(',', $columns);
+
+            unset($columns_arr[4]);
+            $columns = implode(",", $columns_arr);
+            $columns = "draw_record.id,users.phone,draw_record.order_num,draw_record.account,draw_record.reality_account,draw_record.type,draw_record.status,draw_record.refuse,draw_record.bank_name,draw_record.bank_number,draw_record.bank_owner,draw_record.bank_branch,draw_record.ali_name,draw_record.ali_image,draw_record.wx_name,draw_record.wx_image,draw_record.create_time";
+//            print_r($worksheet);
+//            exit();
+//            print_r($where);
+//            exit();
+            $sql = $this->model
+//                ->with(['role'])
+                ->with(['currency', 'users'])
+                ->field($columns)
+                ->where($where)
+                ->where($whereIds)
+//                ->fetchSql()
+//                ->select();
+//            print_r($sql);
+//            die;
+//                ->buildsql();
+//            var_dump($sql);
+                ->chunk(100, function ($items) use (&$list, &$line, &$worksheet) {
+                    $styleArray = array(
+                        'font' => array(
+                            'color' => array('rgb' => '000000'),
+                            'size' => 12,
+                            'name' => 'Verdana'
+                        ));
+                    $list = $items = collection($items)->toArray();
+
+                    foreach ($items as $key => $v) {
+                        foreach ($v as $k => $ele) {
+                            $tmparray = explode("_text", $k);
+                            if (count($tmparray) > 1) {
+                                $items[$key][$tmparray[0]] = $ele;
+                                unset($items[$key][$k]);
+                            }
+                        }
+                    }
+                    foreach ($items as $index => $item) {
+                        $line++;
+                        $col = 0;
+                        foreach ($item as $field => $value) {
+                            if (is_array($value)) {
+                                $value = implode($value);
+                            }
+                            $worksheet->setCellValueByColumnAndRow($col, $line, $value);
+                            $worksheet->getStyleByColumnAndRow($col, $line)->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_TEXT);
+                            $worksheet->getCellByColumnAndRow($col, $line)->getStyle()->applyFromArray($styleArray);
+                            $col++;
+                        }
+                    }
+                }, 'draw_record.id');
+
+            $first = array_keys($list[0]);
+            foreach ($first as $k => $ele) {
+                $tmparray = explode("_text", $ele);
+                if (count($tmparray) > 1) {
+                    unset($first[$k]);
+                }
+            }
+
+            foreach ($first as $index => $item) {
+                $worksheet->setCellValueByColumnAndRow($index, 1, __($item));
+            }
+            $excel->createSheet();
+            // Redirect output to a client’s web browser (Excel2007)
+            $title = date("YmdHis");
+            header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+            header('Content-Disposition: attachment;filename="' . $title . '.xlsx"');
+            header('Cache-Control: max-age=0');
+            // If you're serving to IE 9, then the following may be needed
+            header('Cache-Control: max-age=1');
+            // If you're serving to IE over SSL, then the following may be needed
+            header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+            header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT'); // always modified
+            header('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+            header('Pragma: public'); // HTTP/1.0
+            $objWriter = IOFactory::createWriter($excel, 'Xlsx');
+            $objWriter->save('php://output');
+            return;
+        }
     }
 }
