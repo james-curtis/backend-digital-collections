af89f44d69a7c5c8d50665d8d792dc850d19fc08
JamesCurtis <jamescurtis@foxmail.com>
2022-09-21 20:18:11 +0800

    feat: 合成记录导出excel
    link redrose-office/digital-collections#74

--- application/admin/controller/ChipComposeRecord.php
+++ application/admin/controller/ChipComposeRecord.php
@@ -3,6 +3,12 @@
 namespace app\admin\controller;
 
 use app\common\controller\Backend;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 
 /**
  * 碎片合成记录
@@ -11,7 +17,7 @@ use app\common\controller\Backend;
  */
 class ChipComposeRecord extends Backend
 {
-    
+
     /**
      * ChipComposeRecord模型对象
      * @var \app\admin\model\ChipComposeRecord
@@ -35,7 +41,7 @@ class ChipComposeRecord extends Backend
      * 因此在当前控制器中可不用编写增删改查的代码,除非需要自己控制这部分逻辑
      * 需要将application/admin/library/traits/Backend.php中对应的方法复制到当前控制器,然后进行修改
      */
-    
+
 
     /**
      * 查看
@@ -54,15 +60,15 @@ class ChipComposeRecord extends Backend
             list($where, $sort, $order, $offset, $limit) = $this->buildparams();
 
             $list = $this->model
-                    ->with(['goods','users'])
-                    ->where($where)
-                    ->order($sort, $order)
-                    ->paginate($limit);
+                ->with(['goods', 'users'])
+                ->where($where)
+                ->order($sort, $order)
+                ->paginate($limit);
 
             foreach ($list as $row) {
-                
-                $row->getRelation('goods')->visible(['id','name','image']);
-				$row->getRelation('users')->visible(['id','name','nick_name']);
+
+                $row->getRelation('goods')->visible(['id', 'name', 'image']);
+                $row->getRelation('users')->visible(['id', 'name', 'nick_name']);
             }
 
             $result = array("total" => $list->total(), "rows" => $list->items());
@@ -70,6 +76,137 @@ class ChipComposeRecord extends Backend
             return json($result);
         }
         return $this->view->fetch();
+    }
+
+
+    public function export()
+    {
+        if ($this->request->isPost()) {
+            set_time_limit(0);
+            $search = $this->request->post('search');
+            $ids = $this->request->post('ids');
+            $filter = $this->request->post('filter');
+            $op = $this->request->post('op');
+            $columns = $this->request->post('columns');
+
+
+            $excel = new Spreadsheet();
+            $excel->getProperties()
+                ->setCreator("FastAdmin")
+                ->setLastModifiedBy("FastAdmin")
+                ->setTitle("标题")
+                ->setSubject("Subject");
+            $excel->getDefaultStyle()->getFont()->setName('Microsoft Yahei');
+            $excel->getDefaultStyle()->getFont()->setSize(12);
+            $excel->getDefaultStyle()->applyFromArray(
+                array(
+                    'fill' => array(
+                        'type' => Fill::FILL_SOLID,
+                        'color' => array('rgb' => '000000')
+                    ),
+                    'font' => array(
+                        'color' => array('rgb' => "000000"),
+                    ),
+                    'alignment' => array(
+                        'vertical' => Alignment::VERTICAL_CENTER,
+                        'horizontal' => Alignment::HORIZONTAL_CENTER,
+                        'indent' => 1
+                    ),
+                    'borders' => array(
+                        'allborders' => array('style' => Border::BORDER_THIN),
+                    )
+                ));
+
+
+            $worksheet = $excel->setActiveSheetIndex(0);
+            $worksheet->setTitle('标题');
+            $whereIds = $ids == 'all' ? '1=1' : ['chip_compose_record.id' => ['in', explode(',', $ids)]];
+            $this->request->get(['search' => $search, 'ids' => $ids, 'filter' => $filter, 'op' => $op]);
+            list($where, $sort, $order, $offset, $limit) = $this->buildparams();
+            $line = 1;
+            $list = [];
+            $columns_arr = explode(',', $columns);
+
+            unset($columns_arr[4]);
+            $columns = implode(",", $columns_arr);
+            $columns = "chip_compose_record.id,users.nick_name,goods.name,goods.image,chip_compose_record.compose_num,chip_compose_record.createtime";
+//            print_r($worksheet);
+//            exit();
+//            print_r($where);
+//            exit();
+            $sql = $this->model
+//                ->with(['role'])
+                ->with(['users', 'goods'])
+                ->field($columns)
+                ->where($where)
+                ->where($whereIds)
+//                ->fetchSql()
+//                ->select();
+//            print_r($sql);
+//            die;
+//                ->buildsql();
+//            var_dump($sql);
+                ->chunk(100, function ($items) use (&$list, &$line, &$worksheet) {
+                    $styleArray = array(
+                        'font' => array(
+                            'color' => array('rgb' => '000000'),
+                            'size' => 12,
+                            'name' => 'Verdana'
+                        ));
+                    $list = $items = collection($items)->toArray();
+
+                    foreach ($items as $key => $v) {
+                        foreach ($v as $k => $ele) {
+                            $tmparray = explode("_text", $k);
+                            if (count($tmparray) > 1) {
+                                $items[$key][$tmparray[0]] = $ele;
+                                unset($items[$key][$k]);
+                            }
+                        }
+                    }
+                    foreach ($items as $index => $item) {
+                        $line++;
+                        $col = 0;
+                        foreach ($item as $field => $value) {
+                            if (is_array($value)) {
+                                $value = implode($value);
+                            }
+                            $worksheet->setCellValueByColumnAndRow($col, $line, $value);
+                            $worksheet->getStyleByColumnAndRow($col, $line)->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_TEXT);
+                            $worksheet->getCellByColumnAndRow($col, $line)->getStyle()->applyFromArray($styleArray);
+                            $col++;
+                        }
+                    }
+                }, 'chip_compose_record.id');
+
+            $first = array_keys($list[0]);
+            foreach ($first as $k => $ele) {
+                $tmparray = explode("_text", $ele);
+                if (count($tmparray) > 1) {
+                    unset($first[$k]);
+                }
+            }
+
+            foreach ($first as $index => $item) {
+                $worksheet->setCellValueByColumnAndRow($index, 1, __($item));
+            }
+            $excel->createSheet();
+            // Redirect output to a client’s web browser (Excel2007)
+            $title = date("YmdHis");
+            header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+            header('Content-Disposition: attachment;filename="' . $title . '.xlsx"');
+            header('Cache-Control: max-age=0');
+            // If you're serving to IE 9, then the following may be needed
+            header('Cache-Control: max-age=1');
+            // If you're serving to IE over SSL, then the following may be needed
+            header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+            header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT'); // always modified
+            header('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+            header('Pragma: public'); // HTTP/1.0
+            $objWriter = IOFactory::createWriter($excel, 'Xlsx');
+            $objWriter->save('php://output');
+            return;
+        }
     }
 
 }
--- public/assets/js/backend/chip_compose_record.js
+++ public/assets/js/backend/chip_compose_record.js
@@ -40,6 +40,55 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                 ]
             });
 
+
+            var submitForm = function (ids, layero) {
+                var options = table.bootstrapTable('getOptions');
+                var columns = [];
+                $.each(options.columns[0], function (i, j) {
+                    if (j.field && !j.checkbox && j.visible && j.field != 'operate') {
+                        columns.push(j.field);
+                    }
+                });
+                var search = options.queryParams({});
+                $("input[name=search]", layero).val(options.searchText);
+                $("input[name=ids]", layero).val(ids);
+                $("input[name=filter]", layero).val(search.filter);
+                $("input[name=op]", layero).val(search.op);
+                $("input[name=columns]", layero).val(columns.join(','));
+                $("form", layero).submit();
+            };
+            $(document).on("click", ".btn-export", function () {
+                var ids = Table.api.selectedids(table);
+                var page = table.bootstrapTable('getData');
+                var all = table.bootstrapTable('getOptions').totalRows;
+                console.log(ids, page, all);
+                Layer.confirm("请选择导出的选项<form action='" + Fast.api.fixurl($(this).data().exportUrl) + "' method='post' target='_blank'><input type='hidden' name='ids' value='' /><input type='hidden' name='filter' ><input type='hidden' name='op'><input type='hidden' name='search'><input type='hidden' name='columns'></form>", {
+                    title: '导出数据',
+                    btn: ["选中项(" + ids.length + "条)", "本页(" + page.length + "条)", "全部(" + all + "条)"],
+                    success: function (layero, index) {
+                        $(".layui-layer-btn a", layero).addClass("layui-layer-btn0");
+                    }
+                    , yes: function (index, layero) {
+                        submitForm(ids.join(","), layero);
+                        return false;
+                    }
+                    ,
+                    btn2: function (index, layero) {
+                        var ids = [];
+                        $.each(page, function (i, j) {
+                            ids.push(j.id);
+                        });
+                        submitForm(ids.join(","), layero);
+                        return false;
+                    }
+                    ,
+                    btn3: function (index, layero) {
+                        submitForm("all", layero);
+                        return false;
+                    }
+                })
+            });
+
             // 为表格绑定事件
             Table.api.bindevent(table);
         },
